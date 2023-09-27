11d65a10e2d3e1cf83be3f7c2611df7cb71b7896
mportilla <mothaccounts@gmail.com>
2023-09-27 17:46:48 -0300

    cambios finales

--- ReactRouter/src/components/MenuNavBar.jsx
+++ ReactRouter/src/components/MenuNavBar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
 
@@ -11,21 +10,11 @@ const MenuNavBar = () => {
     <>
     <Navbar expand="lg" className="bg-danger">
       <Container>
-        <Navbar.Brand href="#home"></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav, collapsecolor">
           <Nav className="me-auto">
             <Link to="/" className='text-light pe-2 text-decoration-none'>Home</Link>
             <Link to="/Contacto" className='text-light pe-2 text-decoration-none'>Contacto</Link>
           </Nav>
-          <NavDropdown title="HappyCake" id="basic-nav-dropdown-" className='d-flex justify-content-end text-light'>
-              <NavDropdown.Item href="#action/3.1">Rico</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Rico Rico</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Rico Rico Rico</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Rico Rico Rico Rico</NavDropdown.Item>
-            </NavDropdown>
-        </Navbar.Collapse>
+          <Link to="/" className="text-light text-decoration-none d-flex justify-content-end text-light">HappyCake</Link>
       </Container>
     </Navbar>
     </>
