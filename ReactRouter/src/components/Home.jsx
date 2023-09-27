534875297e7e1230f5eb193632b070533de61c85
mportilla <mothaccounts@gmail.com>
2023-09-27 20:16:36 -0300

    emoticons

--- ReactRouter/src/components/Home.jsx
+++ ReactRouter/src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
    <>
     <div>
-        <h1 className='m-4'>Bienvenido a Happy Cake</h1>
+        <h1 className='m-4'>Bienvenido a Happy Cake 🍰</h1>
         <h6 className='m-2'>El lugar de los pasteles</h6>
         <img className='img-fluid, pastel' src="https://static-00.iconduck.com/assets.00/birthday-cake-icon-2048x2048-ulq09lo1.png" alt="Pastel" />
     </div>
--- ReactRouter/src/components/MenuNavBar.jsx
+++ ReactRouter/src/components/MenuNavBar.jsx
@@ -11,10 +11,10 @@ const MenuNavBar = () => {
     <Navbar expand="lg" className="bg-danger">
       <Container>
           <Nav className="me-auto">
-            <Link to="/" className='text-light pe-2 text-decoration-none'>Home</Link>
-            <Link to="/Contacto" className='text-light pe-2 text-decoration-none'>Contacto</Link>
+            <Link to="/" className='text-light pe-2 text-decoration-none'>🏠Home</Link>
+            <Link to="/Contacto" className='text-light pe-2 text-decoration-none'>📒Contacto</Link>
           </Nav>
-          <Link to="/" className="text-light text-decoration-none d-flex justify-content-end text-light">HappyCake</Link>
+          <Link to="/" className="text-light text-decoration-none d-flex justify-content-end text-light">HappyCake🍰</Link>
       </Container>
     </Navbar>
     </>
