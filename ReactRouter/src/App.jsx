41f2265696dd85a58bbcd02b136184c509629671
mportilla <mothaccounts@gmail.com>
2023-09-27 14:11:25 -0300

    estructura navbar

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Home from './components/home'
 import Contacto from './components/Contacto'
+import Notfound from './components/Notfound'
+import MenuNavBar from './components/MenuNavBar'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
  
   return (
     <>
+    <MenuNavBar/>
     <Routes>
       <Route path='/' element= {<Home/>}/>
       <Route path='/Contacto' element= {<Contacto/>}/>
+      <Route path='/*' element= {<Notfound/>}/>
     </Routes>
     </>
   )
